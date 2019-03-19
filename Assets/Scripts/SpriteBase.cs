dc7a075e1abea6bf6ad32ff519227c73791dc04e
Arthur Tavares <arth_tavares@hotmail.com>
2019-03-19 18:07:31 -0300

    Added Sprite reference to preload sprites in the start

---
+++ Assets/Scripts/SpriteBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteBase : MonoBehaviour {
+	public static SpriteBase I;
+	public Sprite bullet;
+	public Sprite shooter;
+	public Sprite shooter2;
+	public Sprite diver;
+	public Sprite wall;
+	public Sprite item;
+	void Start () {
+		I=this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
