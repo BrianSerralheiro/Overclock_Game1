fb8e8d46deb1100fcd26bad5e5f10eba3563b244
Brian Serralheiro <brian.serralheiro@gmail.com>
2019-03-28 18:24:42 -0300

    IntroScene(scene and script) + overclock logo effect

---
+++ Assets/Scripts/IntroScene.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class IntroScene : MonoBehaviour 
+{
+	private float Timer;
+
+	[SerializeField]
+	private RawImage studioLogo;
+
+	private Color introColor;
+
+	// Use this for initialization
+	void Start () 
+	{
+		introColor = studioLogo.color;
+		Timer = 5f;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Timer -= Time.deltaTime;
+		introColor.a = (5 - Timer) / 2;
+		if(introColor.a > 1)
+		{
+			introColor.a = 1;
+		}
+
+		studioLogo.color = introColor;
+		
+		if(Timer <= 0)
+		{
+			SceneManager.LoadScene("MainMenu");
+		}
+	}
+}
