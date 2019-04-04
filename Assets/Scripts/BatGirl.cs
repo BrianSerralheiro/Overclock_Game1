dd2859fa205cc33f5221a1e5274606e7fb41987a
Arthur Tavares <arth_tavares@hotmail.com>
2019-04-04 15:24:47 -0300

    Batgirls launches 3 little bats

--- Assets/Scripts/BatGirl.cs
+++ Assets/Scripts/BatGirl.cs
@@ -11,10 +11,11 @@ public class BatGirl : EnemyBase {
 	private Sprite closed;
 	private Vector3 vector = new Vector3();
 	private Vector3 pos = new Vector3(0.35f,0.66f,0.1f);
-	private float timer;
+	private Vector3[] poses={new Vector3(-1,-1,0).normalized,new Vector3(0,-1,0).normalized,new Vector3(1,-1,0).normalized };
+	private float timer=3;
 	new void Start () {
 		base.Start();
-		hp=50;
+		hp=150;
 		GameObject go = new GameObject("wingL");
 		render=go.AddComponent<SpriteRenderer>();
 		left=SpriteBase.I.batgirl[2];
@@ -27,12 +28,17 @@ public class BatGirl : EnemyBase {
 		wingL.parent=wingR.parent=transform;
 		wingL.localPosition=new Vector3(0.35f,0.66f,0.1f);
 		wingR.localPosition=new Vector3(-0.35f,0.66f,0.1f);
+		go = new GameObject("head");
+		go.AddComponent<SpriteRenderer>().sprite=SpriteBase.I.batgirl[1];
+		go.transform.parent=transform;
+		go.transform.localPosition=new Vector3(0f,0.9f,-0.1f);
 	}
-	
+
 	new void Update(){
 		base.Update();
 		timer-=Time.deltaTime;
 		vector.Set(0,0,Mathf.PingPong(Time.time*300,-45f)+60f);
+		if(transform.position.y>8)transform.Translate(0,-Time.deltaTime*2,0);
 		if(timer<2)
 		{
 			render.sprite=closed;
@@ -45,10 +51,25 @@ public class BatGirl : EnemyBase {
 			render.sprite=left;
 			wingR.gameObject.SetActive(true);
 			pos.z=0.1f;
+			for(int i = 0; i<3; i++)
+			{
+				Bat(i);
+			}
 			timer=5;
 		}
 		wingL.localPosition=pos;
 		wingL.localEulerAngles=vector;
 		wingR.localEulerAngles=-vector;
+	}
+	void Bat(int i)
+	{
+		GameObject go=new GameObject("enemy");
+		go.AddComponent<Bat>().target=transform.position+poses[i]*3f;
+		go.AddComponent<SpriteRenderer>().sprite=SpriteBase.I.bat[0];
+		go.AddComponent<BoxCollider2D>();
+		Rigidbody2D r = go.AddComponent<Rigidbody2D>();
+		r.isKinematic=true;
+		r.useFullKinematicContacts=true;
+		go.transform.position=transform.position;
 	}
 }
