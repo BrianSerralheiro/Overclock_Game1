3c3590d9967cbbfde9e9fefee344590a9eaf9edd
Arthur Tavares <arth_tavares@hotmail.com>
2019-04-13 16:40:19 -0300

    Added bomber to the 4th enemy group

---
+++ Assets/Scripts/Enemies/Bomber.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomber : EnemyBase {
+	private float timer=4;
+	private Vector3 local=new Vector3(-3.5f,-0.2f);
+	new void Start () {
+		base.Start();
+		hp=200;
+	}
+
+	public override void Position(int i)
+	{
+		base.Position(i);
+		if(i<3)
+		{
+			transform.Rotate(0,0,-90f);
+
+		}
+		else 
+		{
+			transform.Rotate(0,0,(i+1)%2*180f);
+		}
+	}
+	new void Update () {
+		base.Update();
+		transform.Translate(Time.deltaTime,0,0);
+		timer-=Time.deltaTime;
+		if(timer<0)Bomb();
+		if(transform.position.x<-4 || transform.position.x>10 || transform.position.y<-4.5)Die();
+
+	}
+	void Bomb()
+	{
+		timer=2;
+		GameObject go = new GameObject("enemy");
+		go.AddComponent<SpriteRenderer>().sprite=SpriteBase.I.bomber[1];
+		go.AddComponent<BoxCollider2D>();
+		Rigidbody2D r = go.AddComponent<Rigidbody2D>();
+		r.isKinematic=true;
+		r.useFullKinematicContacts=true;
+		go.AddComponent<Bomb>();
+		go.transform.parent=transform;
+		go.transform.localPosition=local;
+		go.transform.parent=null;
+	}
+
+}
---
+++ Assets/Scripts/Projectiles/Bomb.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : EnemyBase {
+	private Vector3 pos;
+	private float speed;
+	new void Start () {
+		base.Start();
+		hp=5;
+		pos=transform.position;
+	}
+	
+	new void Update () {
+		base.Update();
+		pos=Vector3.MoveTowards(pos,player.position,Time.deltaTime*speed);
+		speed+=Time.deltaTime;
+		if(speed>5)speed=5;
+		transform.position=pos;
+		transform.Rotate(0,0,Time.deltaTime*90);
+	}
+	public new void OnCollisionEnter2D(Collision2D col)
+	{
+		if(col.gameObject.name.Contains("Ship")) Explode();
+		base.OnCollisionEnter2D(col);
+	}
+	protected override void Die()
+	{
+		Destroy(gameObject);
+	}
+	private void Explode()
+	{
+		Destroy(gameObject);
+		Debug.Log("BOOM");
+	}
+}
