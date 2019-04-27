77a520f4f6f8b18257d0610706e169e038feda9d
Arthur Tavares <arth_tavares@hotmail.com>
2019-04-27 17:14:07 -0300

    Added particle manager for scene particle effects

---
+++ Assets/Scripts/Game/ParticleManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleManager : MonoBehaviour {
+	[SerializeField]
+	private ParticleSystem[] systems;
+	private static ParticleSystem[] sys;
+	void Start () {
+		sys=systems;
+	}
+
+	public static void Emit(int i,Transform t,int c)
+	{
+		sys[i].transform.position=t.position;
+		sys[i].Emit(c);
+	}
+	public static void Emit(int i,Vector3 p,int c)
+	{
+		sys[i].transform.position=p;
+		sys[i].Emit(c);
+	}
+}
