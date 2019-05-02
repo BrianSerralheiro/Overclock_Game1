70d674d1bd2726bab55ab8081b000320ae7232b5
Arthur Tavares <arth_tavares@hotmail.com>
2019-05-02 00:10:56 -0300

    Updated to make fill fir the healthbars acordingly

--- Assets/Scripts/UI/InGame_HUD.cs
+++ Assets/Scripts/UI/InGame_HUD.cs
@@ -11,28 +11,32 @@ public class InGame_HUD : MonoBehaviour
 	public static float shipHealth = 1;
 
 	[SerializeField]
+	private RectTransform fillMask;
 	private RectTransform lifeFill;
-	
+
 	public static float _special;
 
 	[SerializeField]
 	private RectTransform specialFill;
-
-	// Use this for initialization
+	Vector3 helper=Vector3.one;
+	
 	void Start () 
 	{
-		
+		lifeFill=fillMask.GetChild(0) as RectTransform;
 	}
 	
-	// Update is called once per frame
 	void Update () 
 	{
-		lifeFill.localScale = Vector3.right * shipHealth + Vector3.up;
+		helper.x=shipHealth;
+		fillMask.localScale =helper;
+		helper.x=1f/shipHealth;
+		lifeFill.localScale=helper;
 		scoreHUD.text = EnemySpawner.points.ToString();
 		if(_special > 1)
 		{
 			_special = 1;
 		}
-		specialFill.localScale = Vector3.right * _special + Vector3.up;
+		helper.x=_special;
+		specialFill.localScale =helper;
 	}
 }
