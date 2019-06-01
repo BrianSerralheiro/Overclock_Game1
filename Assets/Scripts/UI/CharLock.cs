8e70627ea813a6bb40a3de70b6f15b51c7169be6
Brian Serralheiro <brian.serralheiro@gmail.com>
2019-06-01 10:25:17 -0700

    Skins and Characters unlockable UI elements placed but not tested

---
+++ Assets/Scripts/UI/CharLock.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharLock : MonoBehaviour {
+
+	[SerializeField]
+	private int charId;
+	void OnEnable()
+	{
+		Button b = GetComponent<Button>();
+		if(b)
+		{
+			 b.interactable=Locks.Char(charId);
+		}
+		else Debug.LogError("CharLock needs a button to work");
+	}
+}
