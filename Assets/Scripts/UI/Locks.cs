c30e141680527057c49f4d7ddf963ba49525c461
Arthur Tavares <arth_tavares@hotmail.com>
2019-06-05 22:56:04 -0300

    Added function to unlock all skins/characters and mark a premium for extra features

--- Assets/Scripts/UI/Locks.cs
+++ Assets/Scripts/UI/Locks.cs
@@ -6,11 +6,12 @@ public static class Locks {
 	
 	private static string skins="000000000000";
 	private static string chars="000";
+	private static bool premium;
 
 	public static void Load()
 	{
-		if(PlayerPrefs.GetString("skins")!="")skins=PlayerPrefs.GetString("skins");
-		if(PlayerPrefs.GetString("chars")!="")skins=PlayerPrefs.GetString("chars");
+		if(PlayerPrefs.HasKey("skins"))skins=PlayerPrefs.GetString("skins");
+		if(PlayerPrefs.HasKey("chars"))skins=PlayerPrefs.GetString("chars");
 
 	}
 	public static void Save()
@@ -35,5 +36,18 @@ public static class Locks {
 	{
 		chars=chars.Substring(0,i)+"1"+chars.Substring(i+1,2-i);
 		Save();
+	}
+	public static void UnlockAll()
+	{
+		for(int i = 0; i<skins.Length; i++)
+		{
+			if(i<chars.Length)Char(i);
+			Skin(i);
+		}
+		premium=true;
+	}
+	public static bool IsPremium()
+	{
+		return premium;
 	}
 }
