f02e86ba3a7096576e1bf9cb4f3c2754280d1253
Arthur Tavares <arth_tavares@hotmail.com>
2019-07-14 17:32:59 -0300

    Buffed MFBat to its true godlike power

--- Assets/Scripts/Enemies/MFBat.cs
+++ Assets/Scripts/Enemies/MFBat.cs
@@ -3,51 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MFBat : EnemyBase {
-	private bool avoid;
 	private float timer=5;
-	private int ap;
 	private float sprite;
+	private Vector2 dir;
 	new void Start () {
 		base.Start();
+		dir=new Vector3(Random.Range(-1f,1f),-0.5f);
 		explosionID = 10;
-		hp=2000;
-		points=20000;
+		hp=150;
+		points=150;
 	}
 	
 	new void Update ()
 	{
-		//if(Ship.paused) return;
+		if(Ship.paused) return;
 		base.Update();
+		dir+=Random.insideUnitCircle/10f;
 		if(timer>0)timer-=Time.deltaTime;
+		else Shoot();
 		if(sprite>0)sprite-=Time.deltaTime;
 		_renderer.flipX=transform.position.x<player.position.x;
-		transform.Translate(0,-Time.deltaTime/2,0);
+		if(transform.position.x>Scaler.sizeX/2-1)dir.x=-Mathf.Abs(dir.x);
+		if(transform.position.x<-Scaler.sizeX/2+1)dir.x=Mathf.Abs(dir.x);
+		if(transform.position.y>Scaler.sizeY-2)dir.y=-Mathf.Abs(dir.y);
+		if(transform.position.y<-Scaler.sizeY-2)Die();
+		transform.Translate(dir.x*Time.deltaTime,dir.y*Time.deltaTime,0);
 		if(sprite<=0)_renderer.sprite=SpriteBase.I.MFBat[Mathf.RoundToInt(Mathf.PingPong(Time.time*3,1f))];
-		if(avoid)
-		{
-			if(timer<0)
-			{
-				timer=0.2f;
-				transform.position=Random.insideUnitCircle*4;
-				if(ap++>7){
-					avoid=false;
-					timer=3;
-					ap=0;
-				}
-			}
-		}
-		else
-		{
-			if(timer<0)
-			{
-				timer=1;
-				if(ap++==0)transform.position=player.position+Vector3.up*5;
-				Shoot();
-				if(ap>2){
-					avoid=true;
-					ap=0;}
-			}
-		}
+		
 	}
 	void Shoot()
 	{
@@ -62,6 +44,7 @@ public class MFBat : EnemyBase {
 		go.transform.localScale=Vector3.right*3+Vector3.up*2;
 		sprite=0.3f;
 		_renderer.sprite=SpriteBase.I.MFBat[2];
+		timer=0.8f;
 		//go.transform.localScale=Vector3.one*2;
 	}
 }
